91caa0e60adc2cd724b74ad5c03e2e154db9afbe
Waldo Plasmatics <mvdssel1@outlook.com>
2023-12-04 14:38:52 +0100

    day 4: part 1

--- d04/d04.py
+++ d04/d04.py
@@ -1,5 +1,25 @@
 INPUT = 'd04/input-test.txt'
 INPUT = 'd04/input.txt'
 
+sumCardPoints = 0
+
 with open(INPUT) as f:
-    for line in f:
+    for card in f:
+        cardTitle, cardNumbers = card.split(':')
+
+        winningNumbers, myNumbers = cardNumbers.split('|')
+        winningNumbers = set((int(n) for n in winningNumbers.split()))
+        myNumbers = set((int(n) for n in myNumbers.split()))
+        
+        cardPoints = 0
+
+        for myNumber in myNumbers:
+            if myNumber in winningNumbers:
+                if cardPoints == 0:
+                    cardPoints = 1
+                else:
+                    cardPoints *= 2
+        
+        sumCardPoints += cardPoints
+
+print('There are {} card points.'.format(sumCardPoints))
